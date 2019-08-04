4d4b0afbe40c8f7d9b5a23f4b67f8a7b96cc98d7
scottsilver <scottsilver47@gmail.com>
2019-08-03 19:11:35 -0500

    wrote function to handlefield change and created form for title and subtitle

--- src/components/stories/EditStory.js
+++ src/components/stories/EditStory.js
@@ -15,6 +15,33 @@ export default class EditStory extends Component {
         this.setState(stateToChange)
       }
 
+render () {
+    <React.Fragment>
+    <form className="storyForm">
+              <div className="form-group">
+                <label htmlFor="storyTitle">Title</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  onChange={this.handleFieldChange}
+                  id="storyTitle"
+                  value={this.state.storyTitle}
+                  />
+                  </div>
+                <div className="form-group">
+                <label htmlFor="subTitle">subtitle</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  onChange={this.handleFieldChange}
+                  id="subtitle"
+                  value={this.state.subtitle}
+                  />
+                  </div>
 
+                  </form>
+
+    </React.Fragment>
+}
 
 }
